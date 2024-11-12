fc7b880169ef40be791f8719c3b071da693c8152
Anwar766 <96235580+Anwar766@users.noreply.github.com>
2024-11-12 18:26:30 +0100

    ferdig

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -2,14 +2,29 @@ package no.hvl.dat100.oppgave4;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-
-import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave3.*;
-
+import java.io.File;
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
-
-		throw new UnsupportedOperationException(TODO.method());
-	}
-}
+		PrintWriter writer = null;
+		
+		try {
+			
+			File fil = new File(mappe + "/" + filnavn);
+			writer = new PrintWriter(fil);
+			
+			
+			writer.print(samling.toString());
+			return true;
+		}catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		}finally {
+			
+			if(writer !=null) {
+				writer.close();
+			}
+	   }
+    }
+ }
